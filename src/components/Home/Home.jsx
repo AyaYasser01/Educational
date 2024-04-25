785ef169c07f6747459f025d837da1ad0a1a10e8
Ayayasser <Ayayasser920@gmail.com>
2024-04-25 14:25:16 +0200

    initial

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './hero/Hero'
+import AboutCard from '../About/AboutCard'
+import HAbout from './HAbout'
+import Testimonal from './testimonal/Testimonal'
+import Hblog from './Hblog'
+import Hprice from './Hprice'
+
+function Home() {
+  return (
+    <>
+      <Hero/>
+      <AboutCard />
+      <HAbout />
+      <Testimonal />
+      <Hblog />
+      <Hprice/>
+    </>
+  )
+}
+
+export default Home
